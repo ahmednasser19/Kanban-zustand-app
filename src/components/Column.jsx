09cd018cc9446c39716971150a24627785da48fa
ahmed nasser <ahmednas197@gmail.com>
2023-06-09 16:42:44 +0300

    displaying tasks

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,11 +1,21 @@
+import { useStore } from "../store";
 import "./Column.css";
 import Task from "./Task";
+import { useMemo } from "react";
 
 export default function Column({ state }) {
+  const tasks = useStore((store) => store.tasks);
+  //this is a solution for  not re-rendering the tasks
+  const filteredTasks = useMemo(
+    () => tasks.filter((task) => task.state === state),
+    [tasks, state]
+  );
   return (
     <div className="column">
       <p>{state}</p>
-      <Task title="Task 1" />
+      {filteredTasks.map((task) => (
+        <Task title={task.title} key={task.title} />
+      ))}
     </div>
   );
 }
